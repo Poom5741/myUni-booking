0d30e4fb385cecce6d8f357df5250b661ed741b3
poom <root@Pooms-MacBook-Pro.local>
2023-09-11 23:49:01 +0700

    fix all npm run build failed

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,97 +2,102 @@ import type { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import https from 'https';
 
+// Define a type for the expected shape of the API response
+interface ApiResponse {
+  status: boolean;
+  username: string; // Add other properties as needed
+}
+
 // Define a function to make the API call
-async function makeApiCall(username, password) {
-    const options = {
-        method: 'POST',
-        hostname: 'restapi.tu.ac.th',
-        path: '/api/v1/auth/Ad/verify',
-        headers: {
-            'Content-Type': 'application/json',
-            'Application-Key': 'TU7f11b30d67bd70bfa415db7e6352c8076e04bf9536a9e2ed7416e209d1e7896cd8da668be8ac169f93d35e44963b62e5'
-        }
-    };
+async function makeApiCall(username: string, password: string): Promise<ApiResponse> {
+  const options = {
+    method: 'POST',
+    hostname: 'restapi.tu.ac.th',
+    path: '/api/v1/auth/Ad/verify',
+    headers: {
+      'Content-Type': 'application/json',
+      'Application-Key': process.env.TU_API_SECRET as string,
+    },
+  };
 
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            const chunks = [];
+  return new Promise<ApiResponse>((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      const chunks: Buffer[] = []; // Provide explicit type information here
 
-            res.on("data", (chunk) => {
-                chunks.push(chunk);
-            });
+      res.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
 
-            res.on("end", () => {
-                const body = Buffer.concat(chunks);
-                const response = JSON.parse(body.toString());
-                resolve(response);
-            });
+      res.on("end", () => {
+        const body = Buffer.concat(chunks);
+        const response = JSON.parse(body.toString()) as ApiResponse;
+        resolve(response);
+      });
 
-            res.on("error", (error) => {
-                console.error(error);
-                reject(error);
-            });
-        });
-
-        const postData = JSON.stringify({
-            UserName: username,
-            PassWord: password
-        });
-
-        req.write(postData);
-        req.end();
+      res.on("error", (error) => {
+        console.error(error);
+        reject(error);
+      });
     });
+
+    const postData = JSON.stringify({
+      UserName: username,
+      PassWord: password,
+    });
+
+    req.write(postData);
+    req.end();
+  });
 }
 
 export const options: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: {
-                    label: "Username:",
-                    type: "text",
-                    placeholder: "your-cool-username"
-                },
-                password: {
-                    label: "Password:",
-                    type: "password",
-                    placeholder: "your-awesome-password"
-                }
-            },
-            async authorize(credentials) {
-                try {
-                    const apiResponse = await makeApiCall(credentials.username, credentials.password);
-                    
-                    if (apiResponse.status === true) {
-                        // Extract relevant user information and create the user object
-                        const {
-                            username,
-                            displayname_th,
-                            displayname_en,
-                            email,
-                            department,
-                            faculty,
-                        } = apiResponse;
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.username || !credentials.password) {
+          // Credentials are missing or incomplete
+          throw new Error('Missing credentials');
+        }
 
-                        const user = {
-                            id: username,
-                            name: displayname_th || displayname_en,
-                            email,
-                            department,
-                            faculty,
-                        };
+        try {
+          // Use the type assertion 'as ApiResponse' to specify the expected shape
+          const apiResponse = await makeApiCall(credentials.username, credentials.password) as ApiResponse;
 
-                        return Promise.resolve(user);
-                    } else {
-                        throw new Error('Authentication failed');
-                    }
-                } catch (error) {
-                    console.error(error);
-                    throw new Error('Authentication failed');
-                }
-            }
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET as string,
+          if (apiResponse.status === true) {
+            // Extract relevant user information and create the user object
+            const {
+              username,
+              // Add other properties as needed
+            } = apiResponse;
+
+            const user = {
+              id: username,
+              // Add other user properties as needed
+            };
+
+            return Promise.resolve(user);
+          } else {
+            throw new Error('Authentication failed');
+          }
+        } catch (error) {
+          console.error(error);
+          throw new Error('Authentication failed');
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string,
 };
